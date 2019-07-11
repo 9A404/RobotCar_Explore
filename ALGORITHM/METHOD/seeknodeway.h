a9aa80823326d417a5e52db7d9084b4d4daeb87d
panshao <panshao96@hotmail.com>
2019-07-11 13:49:15 +0800

    first.

---
+++ ALGORITHM/METHOD/seeknodeway.h
@@ -0,0 +1,26 @@
+#ifndef _SEEKNODEWAY_H_
+#define _SEEKNODEWAY_H_
+#include "sys.h"
+#include "myconfig.h"
+
+u8 seekNodeMethod_default(void);
+u8 seekNodeMethod_4(void);
+u8 seekNodeMethod_10(void);
+u8 seekNodeMethod_pesL(void);
+u8 seekNodeMethod_pesR(void);
+u8 seekNodeMethod_pesPlatform(void);
+u8 seekNodeMethod_Collision(void);
+u8 seekNodeMethod_SeeSaw(void);
+u8 seekNodeMethod_digR(void );
+u8 seekNodeMethod_digL(void);
+
+#endif
+
+
+
+
+
+
+
+
+
